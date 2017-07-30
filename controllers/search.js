d54cf145d2078d761390760e23b901429c9a3f03
YG777 <katyake@gamil.com>
2017-07-30 09:56:31 +0100

    -m "some changes in api search"

--- controllers/search.js
+++ controllers/search.js
@@ -9,46 +9,56 @@ function searchApi(searchTerm, callback) {
   };
 
   http.request(options, function (res) {
+    // retrieve data chunks and create a string representation of them
     var body = '';
     res.on('data', function (chunk) {
       body += chunk;
     });
 
     res.on('end', function () {
-      var response = JSON.parse(body);
+      //convert data to json object
+      var jsonResponse = JSON.parse(body);
       var wines = [];
-      for(var i = 0; i < response.Products.List.length; i++){
-        wines.push(convertObj(response.Products.List[i]));
+      //loop through teh wines in the response and convert them into our view model
+      for(var i = 0; i < jsonResponse.Products.List.length; i++){
+        wines.push(convertObj(jsonResponse.Products.List[i]));
       }
+      //return the converted view models
       callback(wines);
     });
   }).end();
-
-  //return wine;
 }
 
-function convertObj(fullWine) {
+//this function creates an object that is used as a view model
+//the data is populated there
+function convertObj(jsonApiWine) {
   var wine = {};
-  wine.Id = fullWine.Id;
-  wine.Name = fullWine.Name;
-  wine.Country = fullWine.Appellation.Name;
-  wine.ImgLink = fullWine.Labels[0].Url;
-  wine.Grape = fullWine.Varietal.Name;
-  wine.TypeOrColor = fullWine.Varietal.WineType.Name;
-  wine.Vineyard = fullWine.Vineyard.Name;
-  wine.Vintage = fullWine.Vintage;
-  wine.Description = wine.Description;
+  wine.Id = jsonApiWine.Id;
+  wine.Name = jsonApiWine.Name;
+  wine.Country = jsonApiWine.Appellation.Name;
+  wine.ImgLink = jsonApiWine.Labels[0].Url;
+  wine.Grape = jsonApiWine.Varietal.Name;
+  wine.TypeOrColor = jsonApiWine.Varietal.WineType.Name;
+  wine.Vineyard = jsonApiWine.Vineyard.Name;
+  wine.Vintage = jsonApiWine.Vintage;
+  wine.Description = jsonApiWine.Description;
 
   return wine;
 }
 
 function searchWine(req, res){
+  //get the search term from the querystring
   var searchTerm = req.query.searchTerm;
-  function callback(results){
+
+  //searchApi takes a callback function
+  //this will render the view with the view model when it is called
+  function callback(searchResults){
     res.render('search/results', {
-      wines: results
+      wines: searchResults
     });
   }
+  
+  //call API and render view when done
   searchApi(searchTerm, callback);
 }
 
